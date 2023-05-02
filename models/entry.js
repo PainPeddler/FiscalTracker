602eb6e554844a85cb9a55193a2804543c9bee08
John Metzger <jsmetzger9@gmail.com>
2023-05-01 21:28:50 -0400

    initial models

---
+++ models/entry.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Entry extends Model {}
+
+Entry.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        entry_type: {// expense or income?
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        amount: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+        }
+    }
+)
