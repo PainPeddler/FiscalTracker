89f17c353745ab5af7562eaff5ff5c09e91d2fbc
Jacob Sniff <jacobsniff18@gmail.com>
2023-05-01 20:08:38 -0400

    database set up and connected to app using sequelize and dotenv.

---
+++ app.js
@@ -0,0 +1,10 @@
+const db = require('./models/db');
+
+// Syncs the models with the database and logs wether is sussessful or if there is an error
+db.sync()
+    .then(() => {
+        console.log('Database synced successfully')
+    })
+    .catch((error) => {
+        console.log('Error syncing database:', error);
+    });
