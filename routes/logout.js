60c6c9ede9891f5499a54cb1c3ac3b3e36e3c34c
Jacob Sniff <jacobsniff18@gmail.com>
2023-05-04 20:01:42 -0400

    moved routes to controllers folder using mini project template and commented out routes folder.

--- routes/logout.js
+++ routes/logout.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-router.post('/', (req, res) => {
-    // Destroy session to logout user
-    req.session.destroy((err) => {
-        if (err) {
-            console.error(err);
-        }
-        // Redirect the user to the homepage after logout
-        res.redirect('/');
-    });
-});
+// router.post('/', (req, res) => {
+//     // Destroy session to logout user
+//     req.session.destroy((err) => {
+//         if (err) {
+//             console.error(err);
+//         }
+//         // Redirect the user to the homepage after logout
+//         res.redirect('/');
+//     });
+// });
 
-module.exports = router;
+// module.exports = router;
