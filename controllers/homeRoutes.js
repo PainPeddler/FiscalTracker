319cf1d5bec7d8b44d95709832654a6725a8f6cd
Jacob Sniff <jacobsniff18@gmail.com>
2023-05-09 16:33:28 -0400

    Added navbar to mainpage and events container. refined a little with css.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    res.render('homepage')
+router.get("/", (req, res) => {
+  res.render("homepage");
 });
 
-router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect('/profile');
-      return;
-    }
-  
-    res.render('login');
-  });
-  
-  module.exports = router;
-  
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
