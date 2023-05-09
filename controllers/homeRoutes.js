4567a2ded4c533a4adfb184c2146c87821ebb4bb
John Metzger <jsmetzger9@gmail.com>
2023-05-09 17:18:04 -0400

    updated profile .js with charts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,6 +13,10 @@ router.get('/login', (req, res) => {
   
     res.render('login');
   });
+  router.get('/profile', (req, res) => {
+  
+    res.render('profile');
+  });
   
   module.exports = router;
   
